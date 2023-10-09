9972e54629d04856570c81ed53521d51bb0697a5
silkaay <silke.pajenkamp@gmail.com>
2023-10-10 01:31:16 +0200

    Formulare teilweise erstellt

---
+++ login.js
@@ -0,0 +1,6 @@
+document.getElementById("buttonzurückLogin").addEventListener("click", function() {
+    window.location.href = "login.html";
+});
+document.getElementById("buttonNeuerAccount").addEventListener("click", function() {
+    window.location.href = "loginErstellen.html";
+});
