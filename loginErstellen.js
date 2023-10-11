466ae0ae6cdad84b53364b9c526889d42634868d
silkaay <silke.pajenkamp@gmail.com>
2023-10-11 03:00:53 +0200

    Registierungsform ist erstellt

--- loginErstellen.js
+++ loginErstellen.js
@@ -1,3 +1,16 @@
 document.getElementById("buttonzurückLogin").addEventListener("click", function() {
     window.location.href = "login.html";
 });
+
+function toggleForm(formId) {
+    var privatForm = document.getElementById('privatForm');
+    var orgaForm = document.getElementById('orgaForm');
+
+    if (formId === 'privatForm') {
+        privatForm.style.display = 'block';
+        orgaForm.style.display = 'none';
+    } else if (formId === 'orgaForm') {
+        privatForm.style.display = 'none';
+        orgaForm.style.display = 'block';
+    }
+}
