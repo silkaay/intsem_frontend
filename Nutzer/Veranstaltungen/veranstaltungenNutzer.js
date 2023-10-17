36eaf4c4c58832e33380ec88f94c07ca4af98131
Linus-Kaempfer <linuskaempfer@gmail.com>
2023-10-17 20:53:33 +0200

    verbesserungen

--- Nutzer/Veranstaltungen/veranstaltungenNutzer.js
+++ Nutzer/Veranstaltungen/veranstaltungenNutzer.js
@@ -103,6 +103,31 @@ function renderVeranstaltungsgruppenDetails(veranstaltungsgruppen) {
     if (veranstaltungsgruppen && veranstaltungsgruppen.veranstaltungen) {
         const veranstaltungen = veranstaltungsgruppen.veranstaltungen;
 
+        // Füge die Veranstaltungsgruppen-Information am Anfang ein
+    const gruppenCard = document.createElement('div');
+    gruppenCard.classList.add('col-md-12');
+    gruppenCard.innerHTML = `
+        <div class="card mt-3">
+            <div class="row no-gutters">
+                <div class="col-md-2 d-flex align-items-center">
+                    <!-- Hier kannst du ein Bild für die Veranstaltungsgruppe einfügen -->
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h3 class="card-title">${veranstaltungsgruppen.titel}</h3>
+                        <p class="card-text">${veranstaltungsgruppen.beschreibung}</p>
+                        <!-- Weitere Informationen zur Veranstaltungsgruppe hier einfügen -->
+                    </div>
+                </div>
+                <div class="col-md-2 d-flex align-items-center">
+                    <!-- Füge hier den entsprechenden Button für die Veranstaltungsgruppendetails ein -->
+                </div>
+            </div>
+        </div>
+    `;
+
+    container.appendChild(gruppenCard);
+    
         veranstaltungen.forEach(veranstaltung => {
             if (veranstaltung.isVeroeffentlicht) {
                 // Hier den Code anpassen, um die Veranstaltungen anzuzeigen.
