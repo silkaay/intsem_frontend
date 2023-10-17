a7d15ef7d83b0b5d13c32ee065c0c09b8eefbe70
silkaay <silke.pajenkamp@gmail.com>
2023-10-18 01:47:48 +0200

    PW can be resetted

--- Login/loginVergessen.js
+++ Login/loginVergessen.js
@@ -1,3 +1,39 @@
 document.getElementById("buttonzurückLogin").addEventListener("click", function() {
     window.location.href = "login.html";
 });
+
+function forgotPassword() {
+    const email = document.getElementById('email').value;
+
+    const jsonData = {
+        email: email
+    };
+
+    const url = 'http://localhost:8080/resetPassword';
+
+    // Die Optionen für den Fetch-Request, einschließlich der Methode (POST), Headers und Body
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        credentials: "include",
+        body: JSON.stringify(jsonData) // Das JSON-Objekt als String senden
+    };
+
+    // Den Fetch-Request durchführen, ohne auf die Antwort zu warten
+    fetch(url, requestOptions)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            if (response.ok) {
+                console.log("alles gut");
+            }
+        })
+        .catch(error => {
+            // Fehlerbehandlung, falls der Request fehlschlägt
+            console.error('Error:', error);
+        });
+
+}
