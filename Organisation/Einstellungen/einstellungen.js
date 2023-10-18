683c532b4b881d1f7dd723bbd9c42debb1c26d1e
Linus-Kaempfer <linuskaempfer@gmail.com>
2023-10-18 16:37:33 +0200

    angepasst

--- Organisation/Einstellungen/einstellungen.js
+++ Organisation/Einstellungen/einstellungen.js
@@ -17,11 +17,11 @@ function getLoggedInUser() {
     })
         .then(response => response.json())
         .then(data => {
-            document.getElementById("vorname").textContent = "Vorname: " + data.vorname;
-            document.getElementById("nachname").textContent = "Nachname: " + data.nachname;
+            document.getElementById("name").textContent = "Name: " + data.name;
             document.getElementById("anschrift").textContent = "Anschrift: " + data.anschrift.strasse + " " + data.anschrift.hausnummer + ", " + data.anschrift.plz + " " + data.anschrift.ort;
-            document.getElementById("telefon").textContent = "Telefonnummer: " + data.telefonnummer;
-            document.getElementById("email").textContent = "Email: " + data.email;
+            document.getElementById("vornameAnsprech").textContent = "Vorname Ansprechpartner: " + data.ansprechpartnerVorname;
+            document.getElementById("nachnameAnsprech").textContent = "Nachname Ansprechpartner: " + data.ansprechpartnerNachname;
+            document.getElementById("telefonAnsprech").textContent = "Telefon Ansprechpartner: " + data.telefonnummer;
         })
         .catch(error => {
             console.error('Fehler beim Abrufen der Veranstaltungsgruppen-Details:', error);
